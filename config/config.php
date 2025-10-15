7bc2113910ee39749c5e6cf94a98b24a2f570480
GueldenBayar <g.bayar@hotmail.de>
2025-10-15 11:57:55 +0200

    bugfixes

--- config/config.php
+++ config/config.php
@@ -9,11 +9,11 @@ $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES    => false,
+    PDO::ATTR_EMULATE_PREPARES    => false
 ];
 
 try {
     $pdo = new PDO($dsn, $dbUser, $dbPass, $options);
 } catch (\PDOException $e) {
-    throw new \PDOException(($e->getMessage(), (int)$e->getCode()));
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
--- src/pokemon/delete.php
+++ src/pokemon/delete.php
@@ -1,1 +1,11 @@
 <?php
+
+$id = $_GET['id'] ?? null;
+
+if ($id) {
+    $stmt = $pdo->prepare("DELETE FROM pokemon WHERE id = ?");
+    $stmt->execute([$id]);
+}
+
+header("Location: index.php");
+exit();
